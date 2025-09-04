c95705765a01688f59db1358205db08cd67fdb7b
calliekarii <holtcallie@gmail.com>
2025-09-03 23:06:02 -0400

    Began building playRound function.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,15 +1,35 @@
 function getComputerChoice() {
-    let computerChoice = Math.ceil(Math.random() * 3);
+    let i = Math.ceil(Math.random() * 3);
 
-    if (computerChoice === 1) {
-        return "Rock";
-    } else if (computerChoice === 2) {
-        return "Paper";
+    if (i === 1) {
+        return "rock";
+    } else if (i === 2) {
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
     return prompt("What will you play?");
 }
+
+function playRound(humanChoice, computerChoice) {
+    const nHumanChoice = humanChoice.toLowerCase();
+
+    if (nHumanChoice === computerChoice) {
+        console.log(`It's a tie, you both played ${nHumanChoice}.`)
+    } else if (((computerChoice = "rock") && (nHumanChoice = "paper")) || ((computerChoice = "paper") && (nHumanChoice = "scissors")) || ((computerChoice = "scissors") && (nHumanChoice = "rock"))) {
+        console.log(`You win, ${nHumanChoice} beats ${computerChoice}!`)
+    } else {
+        console.log(`You lose, ${computerChoice} beats ${nHumanChoice}!`)
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
